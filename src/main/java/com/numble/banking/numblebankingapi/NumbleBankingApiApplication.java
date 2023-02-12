6572e9458895f027dcbcf07bb42e2085d46072ae
thinkp0907 <thinkp0907@gmail.com>
2023-02-12 21:58:48 +0900

    #1 - Numble Spring Boot 를 활용한 Banking API 구현 프로젝트
    프로젝트 진행을 위한 파일 생성 및 Git repo 생성

---
+++ src/main/java/com/numble/banking/numblebankingapi/NumbleBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.numble.banking.numblebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleBankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleBankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/numble/banking/numblebankingapi/NumbleBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.banking.numblebankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
