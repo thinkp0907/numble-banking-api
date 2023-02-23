d690a25300fead9209b2ec2c446a01d051edfb50
thinkp0907 <thinkp0907@gmail.com>
2023-02-23 22:32:06 +0900

    #12 - Global Exception Handler
    추후 Global Exception Handler 를 사용하기 위해 생성해 두었다.

---
+++ src/main/java/com/numble/banking/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.numble.banking.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+}
