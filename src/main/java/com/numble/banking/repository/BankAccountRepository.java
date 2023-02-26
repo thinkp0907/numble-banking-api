ba1670b36ac8f27680fdbaa55d95f03e39e05b53
thinkp0907 <thinkp0907@gmail.com>
2023-02-26 21:12:40 +0900

    #12 - 계좌이체 API 개발 시 사용되는 JPA 메소드 추가
    대상자는 은행이 여러게 있을 수 있고, 또 은행 구분도 동일하게 여러게 있을 수 있기때문에, 은행구분(Div) 와 id로 찾아오는 메소드 하나와
    은행구분과 계좌번호로 가져오는 메소드 하나 추가

--- src/main/java/com/numble/banking/repository/BankAccountRepository.java
+++ src/main/java/com/numble/banking/repository/BankAccountRepository.java
@@ -1,6 +1,7 @@
 package com.numble.banking.repository;
 
 import com.numble.banking.domain.BankAccount;
+import com.numble.banking.dto.BankAccountDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,8 @@ import java.util.List;
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 
     List<BankAccount> findBankAccountsByClientId(String clientId);
+
+    BankAccount findBankAccountByClientIdAndBankDiv(String clientId, String bankDiv);
+
+    BankAccount findBankAccountByBankDivAndAccountNumber(String bankDiv, String accountNumber);
 }
